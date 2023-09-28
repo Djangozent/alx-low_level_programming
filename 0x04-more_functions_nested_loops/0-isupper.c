a3095778e04160e5fccd5def2be0144fc66c44d2
Djangozent <djangozent@gmail.com>
2023-09-28 17:25:55 +0300

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  * _isupper - checks for uppercase character
- * @c: character to be checked
- * Return: 1 if c is uppercase, otherwise 0
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'z');
+	return (c >= 'A' && c <= 'Z');
 }

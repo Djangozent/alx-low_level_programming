09411efc9de18f5d9a5f48bf3a850ca320872aa8
Djangozent <djangozent@gmail.com>
2024-03-25 13:37:48 +0300

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for lowrcase character
+ * @c: the character to check
+ * Return: 1 if i is lowercase, otherwise 0
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
